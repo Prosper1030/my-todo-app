9d1c3e40ef8364ce8e6637b008917bdce72b7f0c
Prosper1030 <1030railgunjudgelight@gmail.com>
2024-08-19 13:55:24 +0800

    upload png, font-size changed.

--- gui.py
+++ gui.py
@@ -6,19 +6,20 @@ sg.theme("black")
 clock = sg.Text("", key="clock")
 lable = sg.Text("Type in to-do")
 input_box = sg.InputText(tooltip="Enter do", key="todo")
-add_button = sg.Button("Add", size=10)
+add_button = sg.Button("Add")
 list_box = sg.Listbox(values=function.get_todo(), key="todos",
-                      enable_events=True, size=[45, 10])
+                      enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
 complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
+
 window = sg.Window('My to do app',
                    layout=[[clock],
                            [lable],
                            [input_box, add_button],
                            [list_box, edit_button, complete_button],
                            [exit_button]],
-                   font=("Helvetica", 20))
+                   font=("Helvetica", 14))
 while True:
     now = time.strftime('%b %d, %Y %H:%M:%S')
     event, value = window.read(timeout=200)
@@ -43,7 +44,7 @@ while True:
                 function.write_todo(todos)
                 window["todos"].update(values=todos)
             except IndexError:
-                sg.popup("Please select an item first", font=("Helvetica", 20))
+                sg.popup("Please select an item first", font=("Helvetica", 14))
         case "Complete":
             try:
                 todo_complete = value["todos"][0]
@@ -53,7 +54,7 @@ while True:
                 window["todos"].update(values=todos)
                 window["todo"].update(value="")
             except IndexError:
-                sg.popup("Please select an item first", font=("Helvetica", 20))
+                sg.popup("Please select an item first", font=("Helvetica", 14))
         case "todos":
             window["todo"].update(value=value["todos"][0].strip("\n"))
 window.close()
